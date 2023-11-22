e58cae493cfeb82656b7f4bbd14f861ccd10f483
ivest76 <ivestrada7601@gmail.com>
2023-11-21 17:43:05 -0800

    starting my app file

---
+++ fetal_app.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+import math
+import warnings
+import pickle
+warnings.filterwarnings('ignore')
+
+st.title("Fetal Health Classification: A Machine Learning App")
+
+st.subhea
